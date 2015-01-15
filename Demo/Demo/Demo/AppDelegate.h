3fd67b6dacffeff50621b86b59051eeac2376928
tgcshta <annorhtht@gmail.com>
2015-01-15 17:01:32 +0900

    Setting.

---
+++ Demo/Demo/Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by sho on 2015/01/15.
+//  Copyright (c) 2015年 sTogu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/Demo/Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by sho on 2015/01/15.
+//  Copyright (c) 2015年 sTogu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
